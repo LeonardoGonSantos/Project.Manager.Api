3dac68899715bf2e1b2df0caff36d9f1fea93442
DevLeo <leonardo.goncalves@jobspay.com.br>
2025-01-21 01:28:47 -0300

    feature: instructions readme

--- Project.Manager.Application/Handlers/Command/Interface/IProjectCommand.cs
+++ Project.Manager.Application/Handlers/Command/Interface/IProjectCommand.cs
@@ -1,8 +1,8 @@
 namespace Project.Manager.Application.Handlers.Command
 {
-public interface IProjectCommand
-{
-    void AddProject(Api.Models.Project project);
-    Api.Models.Project GetProjectById(int projectId);
-}
+    public interface IProjectCommand
+    {
+        void AddProject(Api.Models.Project project);
+        Api.Models.Project GetProjectById(int projectId);
+    }
 }
